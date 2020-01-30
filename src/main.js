b66eb01642b7156aa2e66a3f655115e986a86a8a
cgobbet <cassiano.gobbet@gmail.com>
2020-01-30 10:07:36 +0100

    Inclusion of Material imports

--- src/main.js
+++ src/main.js
@@ -1,10 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
+import VueMaterial from 'vue-material'
 import { MdField } from 'vue-material/dist/components';
 import 'vue-material/dist/vue-material.min.css';
 import 'vue-material/dist/theme/black-green-light.css';
 
 Vue.use(MdField);
+Vue.use(VueMaterial)
 
 Vue.config.productionTip = false
 
