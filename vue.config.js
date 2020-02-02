4fd598ec70c7dda90a5144ea9ed67cff66cb9f42
cgobbet <cassiano.gobbet@gmail.com>
2020-02-02 20:49:39 +0100

    Update output path

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,8 @@
 module.exports = {
-  publicPath: process.env.NODE_ENV === "production" ? "/vue-todo/" : "/"
+  publicPath: "/serve-vue-app",
+  outputDir: "docs"
 };
+
+// module.exports = {
+//   publicPath: process.env.NODE_ENV === "production" ? "/vue-todo/" : "/"
+// };
